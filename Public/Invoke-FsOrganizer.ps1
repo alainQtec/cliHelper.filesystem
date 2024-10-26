128f0f2f543e003ea342ce67aca4125346e8702d
Alain Herve <alainQtec@gmail.com>
2024-10-26 03:01:16 +0200

    Create Invoke-FsOrganizer.ps1

---
+++ Public/Invoke-FsOrganizer.ps1
@@ -0,0 +1,17 @@
+﻿function Invoke-FsOrganizer {
+  # .SYNOPSIS
+  #   A short one-line action-based description, e.g. 'Tests if a function is valid'
+  # .DESCRIPTION
+  #   A longer description of the function, its purpose, common use cases, etc.
+  # .NOTES
+  #   Information or caveats about the function e.g. 'This function is not supported in Linux'
+  # .LINK
+  #   Specify a URI to a help page, this will show when Get-Help -Online is used.
+  # .EXAMPLE
+  #   Invoke-FsOrganizer -Path "C:\Downloads" -Intent "organize my download folder by file type and date"
+  [CmdletBinding()]
+  param (
+  )
+  process {
+  }
+}
